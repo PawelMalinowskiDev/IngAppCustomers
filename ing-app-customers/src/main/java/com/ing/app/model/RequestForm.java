2514a9b7dc9443678d8455a85cca57d0ee597eb8
PawelMalinowskiDev <Pawel@192.168.1.4>
2019-02-10 20:53:27 +1100

    Added RequestForm as a model class. It will be used on a main .jsp page

---
+++ ing-app-customers/src/main/java/com/ing/app/model/RequestForm.java
@@ -0,0 +1,25 @@
+package com.ing.app.model;
+
+public class RequestForm {
+
+	private int customerId;
+
+	private String monthDate;
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getMonthDate() {
+		return monthDate;
+	}
+
+	public void setMonthDate(String monthDate) {
+		this.monthDate = monthDate;
+	}
+
+}
