46b79ff24a945c017eaa1817837990565b400534
PawelMalinowskiDev <Pawel@192.168.1.4>
2019-02-10 22:24:12 +1100

    Added Fast Spender Classification Service.

---
+++ ing-app-customers/src/main/java/com/ing/app/customer/classification/service/impl/FastSpenderClassificationService.java
@@ -0,0 +1,14 @@
+package com.ing.app.customer.classification.service.impl;
+
+import com.ing.app.customer.classification.service.ClassificationStrategyService;
+import com.ing.app.model.Customer;
+
+public class FastSpenderClassificationService implements ClassificationStrategyService{
+
+	@Override
+	public void checkClassification(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
