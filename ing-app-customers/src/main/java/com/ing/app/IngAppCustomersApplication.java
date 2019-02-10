a046e0ce17499c4d672ea99a7441893fc632bf8b
PawelMalinowskiDev <Pawel@192.168.1.4>
2019-02-10 20:20:55 +1100

    Initial Spring Boot simple web project

---
+++ ing-app-customers/src/main/java/com/ing/app/IngAppCustomersApplication.java
@@ -0,0 +1,14 @@
+package com.ing.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngAppCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngAppCustomersApplication.class, args);
+	}
+
+}
+
