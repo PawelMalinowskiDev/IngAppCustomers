60279214e234c860b099e5175c71a9fa78039000
PawelMalinowskiDev <Pawel@192.168.1.4>
2019-02-10 22:26:46 +1100

    Added Potential Saver Classification Service.

---
+++ ing-app-customers/src/main/java/com/ing/app/customer/classification/service/impl/PotentialSaverClassificationService.java
@@ -0,0 +1,14 @@
+package com.ing.app.customer.classification.service.impl;
+
+import com.ing.app.customer.classification.service.ClassificationStrategyService;
+import com.ing.app.model.Customer;
+
+public class PotentialSaverClassificationService implements ClassificationStrategyService{
+
+	@Override
+	public void checkClassification(Customer customer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
