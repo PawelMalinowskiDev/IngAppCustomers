9258d7504a7afeab0d49e32969743517b1ce767c
PawelMalinowskiDev <Pawel@192.168.1.4>
2019-02-10 22:21:04 +1100

    Added based interface for Customer Classification service.

---
+++ ing-app-customers/src/main/java/com/ing/app/customer/classification/service/ClassificationStrategyService.java
@@ -0,0 +1,7 @@
+package com.ing.app.customer.classification.service;
+
+import com.ing.app.model.Customer;
+
+public interface ClassificationStrategyService {
+	public void checkClassification(Customer customer);
+}
